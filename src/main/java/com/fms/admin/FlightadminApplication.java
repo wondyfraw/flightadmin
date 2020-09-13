133b1b832fd0634b93f55aab1a9d0b9307a43940
wondyfraw <wondyfraw@users.noreply.github.com>
2020-09-13 17:28:52 +0200

    first commit

---
+++ src/main/java/com/fms/admin/FlightadminApplication.java
@@ -0,0 +1,13 @@
+package com.fms.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightadminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fms/admin/FlightadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fms.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
